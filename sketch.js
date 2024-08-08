d343490d304e0546bf5b384ed09dc7e59b7da49c
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 08:35:09 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(1000, 1000);
+  background(0,255,0);
+  strokeWeight(10);
+  stroke(0,0,255);
+  line(340,200,200,200);
+  line(200,200,210,600)
+  stroke(255,0,0);
+  point(400,200);
+}
+// Intended Purpose:
+// This is supposed to show what the SNAKE game looks like when paused (stationary).
